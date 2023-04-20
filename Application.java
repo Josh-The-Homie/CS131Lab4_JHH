f0670bc8d4248d901e759c9ca22957b10d6202a1
Josh-Henderson <122053780+Josh-The-Homie@users.noreply.github.com>
2023-04-20 11:35:16 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class Application {
 
+    public static void main(String[] args) {
+        
+        // create a new FileProcessor object with the file name and string length
+        FileProcessor fp = new FileProcessor("GoodString.txt", 5);
+        
+        // test the getters and setters
+        System.out.println("File name: " + fp.getFileName());
+        System.out.println("String length: " + fp.getStringLength());
+        
+        // test the processFile() method
+        fp.processFile();
+        
+    }
+    
 }
